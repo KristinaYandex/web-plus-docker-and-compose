5e99021f581a115c4d8d158dac0ffa19b473ccc2
Kristina Ushakova <mramornayaa@yandex.ru>
2024-02-03 08:37:01 +0300

    Изменила localhost во фронтенде на адрес сайта

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
 /*export const URL = "http://167.235.140.175:3001";*/
-export const URL = "http://localhost:4004";
+export const URL = "http://api.kpd.ushakova.nomoredomainsmonster.ru";
 
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
